aeeb58bc3815d1eb40cd03dc5353f47f03aea25c
Jorge Alamo <jorgealamosan@gmail.com>
2024-06-29 13:26:41 +0100

    WordReader lee documentos word correctamente

--- src/main/java/myprojects/jorgealamoo/wordmodifier/WordReader.java
+++ src/main/java/myprojects/jorgealamoo/wordmodifier/WordReader.java
@@ -1,4 +1,22 @@
 package myprojects.jorgealamoo.wordmodifier;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class WordReader {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("E:\\Usuario\\Documents\\Otros\\documento.docx");
+        XWPFDocument document = new XWPFDocument(fis)) {
+
+            for (XWPFParagraph paragraph : document.getParagraphs()) {
+                System.out.println(paragraph.getText());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error. Document can´t be read");
+        }
+    }
 }
