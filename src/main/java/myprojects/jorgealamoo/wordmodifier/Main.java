a34fee44f7167617e292727f1d5256e489b7ef61
Jorge Alamo <jorgealamosan@gmail.com>
2024-07-02 12:52:01 +0100

    Main de prueba

---
+++ src/main/java/myprojects/jorgealamoo/wordmodifier/Main.java
@@ -0,0 +1,22 @@
+package myprojects.jorgealamoo.wordmodifier;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        WordReader reader = new WordReader("E:\\Usuario\\Documents\\Otros\\documento.docx");
+        Word word = reader.read();
+        reader.close();
+
+        List<String> paragraphs = word.getParagraphs();
+        for (String paragraph : paragraphs){
+            System.out.println(paragraph);
+        }
+
+        word.addParagraph("Nuevo parrafo de prueba añadido al final del documento");
+
+        word.save("E:\\Usuario\\Documents\\Otros\\documento_modificado.docx");
+        word.close();
+    }
+}
